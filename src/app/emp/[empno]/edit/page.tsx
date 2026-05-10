5a21418937c50f8cb5bffb8e48a2a8211a7a9534
youngeiya <kim.ga.young467@gmail.com>
2026-05-11 08:52:57 +0900

    사원관리수정폼 정리

--- src/app/emp/[empno]/edit/page.tsx
+++ src/app/emp/[empno]/edit/page.tsx
@@ -3,7 +3,7 @@ import UpdateForm from "@/components/emp/UpdateForm";
 const EditPage = () => {
     return (
         <div>
-        <p>사원수정페이지입니다.</p>
+    
         <UpdateForm />
         </div>
     );
